0242b9b47743146c2f5066c16b066010e736c13f
Peter Pavlatos <pavlatos.peter@gmail.com>
2022-08-31 14:24:12 -0400

    TBCM-3 added checkbox to components

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,31 +1,12 @@
 import type { NextPage } from 'next';
-import { Button }  from '../components/Button';
+
 import 'reflect-metadata';
 
 
 const Home: NextPage = () => {
   return <>
-      <Button 
-        onClick={()=> alert('Button 1 is clicked !')}
-        variant="secondary"
-        >
-        secondary
-      </Button>
-      <br/> <br/>
-      <Button 
-        onClick={()=> alert('Button 1 is clicked !')}
-        variant="primary"
-        >
-        primary
-      </Button>
-      <br/> <br/>
-      <Button 
-        onClick={()=> alert('Button 1 is clicked !')}
-        variant="outline"
-        >
-        outline
-      </Button>
-      <br/> <br/>
+    <p>Hello World</p>
+      
   </>;
 };
 
