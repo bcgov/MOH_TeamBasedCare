d325ba7c5ae5061440fb3f043d3a4fa37d450150
Peter Pavlatos <pavlatos.peter@gmail.com>
2022-08-31 12:34:43 -0400

    TBCM-3 Altered Button component to a previous working example supplied to me by Karthikk

--- apps/web/src/components/Button.tsx
+++ apps/web/src/components/Button.tsx
@@ -1,32 +1,42 @@
-import React from 'react';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-interface Props {
-  children: React.ReactNode;
-  onClick: () => void;
-  variant?: string, // primary, secondary, tertiary
-  size?: string, // sm, md, lg
+export interface ButtonProps {
+  onClick?: () => void;
+  variant: keyof typeof buttonColor;
+  loading?: boolean;
+  type?: 'submit' | 'reset' | 'button';
   disabled?: boolean;
 }
 
-// button component, consuming props
-const Button: React.FC<Props> = ({
-  children,
-  onClick,
-  variant = 'default',
-  size = 'md',
-  disabled,
-  ...rest
-}) => {
+export const buttonColor: Record<string, string> = {
+  primary: `border-transparent bg-bcBluePrimary text-white hover:bg-blue-800 focus:ring-blue-500`,
+  secondary: `border-2 border-bcBluePrimary bg-white text-bcBluePrimary hover:bg-gray-100 focus:ring-blue-500`,
+  outline: `border border-gray-400 bg-white hover:bg-gray-100 focus:ring-blue-500`,
+};
+
+export const buttonBase = `w-auto inline-flex justify-center items-center rounded 
+  shadow-sm px-4 py-2 text-base font-bold focus:outline-none
+  disabled:opacity-50
+  focus:ring-2 focus:ring-offset-2 sm:mt-0 sm:text-sm`;
+
+export const Button: React.FC<ButtonProps> = props => {
+  const { variant, type, children, disabled, loading, onClick } = props;
   return (
     <button
-      className={`btn ${variant} ${size}` + (disabled ? ' disabled' : '')}
       onClick={onClick}
+      type={type}
+      className={`
+        ${buttonColor[variant]}
+        ${variant !== 'link' ? buttonBase : ''}
+      `}
       disabled={disabled}
-      {...rest}
     >
-      {children}
+      {loading ? (
+        <FontAwesomeIcon icon={faSpinner} className='h-5 w-5 animate-spin anim' />
+      ) : (
+        children
+      )}
     </button>
   );
 };
-
-export default Button;
--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,46 +1,31 @@
 import type { NextPage } from 'next';
-import Button  from '../components/Button';
+import { Button }  from '../components/Button';
 import 'reflect-metadata';
 
 
 const Home: NextPage = () => {
   return <>
-
-    <Button 
-      variant="primary" 
-      onClick={()=>{}}
-      disabled
-      >
-      primary disabled
-    </Button>
-    <br/>
-    <Button 
-      variant="primary" 
-      onClick={()=>{}}
-      >
-      default primary
-    </Button>
-    <br/>
-    <Button 
-      variant="secondary" 
-      onClick={()=>{}}
-      >
-      secondary
-    </Button>
-
-    <br/>
-    <Button 
-      variant="secondary" 
-      onClick={()=>{}}
-      disabled
-      >
-      secondary disabled
-    </Button>
-
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-</h1>
-
+      <Button 
+        onClick={()=> alert('Button 1 is clicked !')}
+        variant="secondary"
+        >
+        secondary
+      </Button>
+      <br/> <br/>
+      <Button 
+        onClick={()=> alert('Button 1 is clicked !')}
+        variant="primary"
+        >
+        primary
+      </Button>
+      <br/> <br/>
+      <Button 
+        onClick={()=> alert('Button 1 is clicked !')}
+        variant="outline"
+        >
+        outline
+      </Button>
+      <br/> <br/>
   </>;
 };
 
