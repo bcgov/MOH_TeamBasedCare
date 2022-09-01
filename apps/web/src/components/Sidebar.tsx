714ee5a0f2f38ed2b8b1b0b11ae38617911559c1
Leon <leon@freshworks.io>
2022-09-01 13:29:39 -0700

    Add Expandable Planning Tab
    - Expands and collapses like in the mockup
    - Collapses fully when the sidebar is collapsed
    
    TODO:
    - Move to their own component to reuse code
    - Some backwards logic on the state - refactor that later (works
      for now)
    - Add appropriate colour coding when highlighted/active (like
      on the mockup).

--- apps/web/src/components/Sidebar.tsx
+++ apps/web/src/components/Sidebar.tsx
@@ -6,6 +6,8 @@ import { faClipboardList, faUsers, faBars } from '@fortawesome/free-solid-svg-ic
 
 export const Sidebar: React.FC = () => {
   const [open, setOpen] = useState(false);
+  const [dropdownOpen, setDropdownOpen] = useState(true);
+
   return (
     <div className='flex overflow-x-hidden h-screen'>
       <aside
@@ -14,7 +16,7 @@ export const Sidebar: React.FC = () => {
         } sidebar fixed top-0 bottom-0 lg:left-0 flex flex-col h-screen p-3 shadow duration-300 bg-bcDarkBlue`}
         aria-label='Sidebar'
       >
-        <div className='space-y-3'>
+        <div className='space-y-3 overflow-y-auto'>
           <div className='flex items-center justify-between'>
             <img
               src={logo.src}
@@ -48,6 +50,32 @@ export const Sidebar: React.FC = () => {
               </a>
             </li>
 
+            <li className='left-0 flex flex-wrap items-center py-4 rounded-sm'>
+              <button
+                type='button'
+                className={`${
+                  open ? 'justify-center' : 'justify-left'
+                } flex items-center p-1 space-x-3 rounded-md`}
+                onClick={() => {
+                  setDropdownOpen(!dropdownOpen);
+                }}
+                aria-controls='dropdownExample'
+                data-collapse-toggle='dropdownExample'
+              >
+                <FontAwesomeIcon className='w-6 h-6 text-gray-100' icon={faClipboardList} />
+
+                <span className={open ? 'hidden' : 'text-gray-100'}>Planning</span>
+              </button>
+              <ul className={dropdownOpen || open ? 'hidden' : 'py-2 space-y-2 w-full'}>
+                <li className='flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700'>
+                  <span className={open ? 'hidden' : 'text-gray-100'}>Create New</span>
+                </li>
+                <li className='flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700'>
+                  <span className={open ? 'hidden' : 'text-gray-100'}>All Plan</span>
+                </li>
+              </ul>
+            </li>
+
             <li className='left-0 flex items-center py-4 rounded-sm'>
               <a
                 href='#'
@@ -55,9 +83,9 @@ export const Sidebar: React.FC = () => {
                   open ? 'justify-center' : 'justify-left'
                 } flex items-center p-1 space-x-3 rounded-md`}
               >
-                <FontAwesomeIcon className='w-6 h-6 text-gray-100' icon={faClipboardList} />
+                <FontAwesomeIcon className='w-6 h-6 text-gray-100' icon={faUsers} />
 
-                <span className={open ? 'hidden' : 'text-gray-100'}>Planning</span>
+                <span className={open ? 'hidden' : 'text-gray-100'}>Test</span>
               </a>
             </li>
           </ul>
