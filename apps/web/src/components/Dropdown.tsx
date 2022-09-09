637b3443023de0c8ca32ebb15c44c2c228748f85
Leon <leon@freshworks.io>
2022-09-09 09:49:59 -0700

    Change Style According to Figma Mockups

--- apps/web/src/components/Dropdown.tsx
+++ apps/web/src/components/Dropdown.tsx
@@ -17,7 +17,7 @@ export const Dropdown = ({ dropdownOptions }: DropdownProps) => {
         <select
           defaultValue='ICU'
           id='careunits'
-          className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
+          className='bg-gray-200 border-b-2 border-gray-900 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
         >
           {dropdownOptions.map((option: DropdownOptionProps) => {
             return (
