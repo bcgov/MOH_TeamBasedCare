6e995c187195df14a485fe08d17502859709acc3
Karthikk Tamil Mani <70780157+karthikktamilmani-freshworks@users.noreply.github.com>
2022-09-09 10:39:49 -0700

    Update axios util hooks

---
+++ apps/web/src/common/request-method.ts
@@ -0,0 +1,12 @@
+export enum REQUEST_METHOD {
+  GET = 'get',
+  POST = 'post',
+  PATCH = 'patch',
+  PUT = 'put',
+  DELETE = 'delete',
+}
+
+export const API_ENDPOINT = {
+  //TODO: remove this endpoint once actual endpoints are added
+  USER: '/user',
+};
