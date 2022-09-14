b9c4c950e76a44895b88107bb41e8abf21843b9e
Peter Pavlatos <pavlatos.peter@gmail.com>
2022-09-14 16:01:21 -0400

    TBCM-34 - Added class to selected Activity. And removed unused code.

--- apps/web/src/components/LeftSideBarActivites.tsx
+++ apps/web/src/components/LeftSideBarActivites.tsx
@@ -1,7 +1,5 @@
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import {  useState } from 'react';
 import _ from 'lodash';
-
 
 export const data =[
     {
@@ -48,6 +46,7 @@ export interface SearchInputProps {
 
 export const LeftSideBarActivites: React.FC<LeftSideBarActivitesProps> = ({ title }) => {
     const [items] = useState(data);
+    const [selectedItem, setSelectedItem] = useState();
     const [searchValue, setSearchValue]: [string, (search: string) => void] = useState("");
 
     // Get search value
@@ -58,6 +57,10 @@ export const LeftSideBarActivites: React.FC<LeftSideBarActivitesProps> = ({ titl
     const filteredData = items.filter(item => {
         return item.name.toLowerCase().includes(searchValue.toLowerCase())
     });
+
+    const handleSetSelectedItem = (id: number) => {
+        setSelectedItem(id)
+    }
 
     return (
         <div className='w-1/3 mt-4 border-2 border-gray-200 p-4'>
@@ -80,7 +83,7 @@ export const LeftSideBarActivites: React.FC<LeftSideBarActivitesProps> = ({ titl
                     { !_.isEmpty(filteredData) ?
                         filteredData.map((item, index) => {
                                 return (
-                                    <li key={index} className="mt-4 hover:bg-sky-700" onClick={()=> alert("Clicked")}>
+                                    <li key={index} className={`${selectedItem == index && "bg-gray-200"} cursor-pointer p-2 hover:bg-gray-200`} onClick={()=> handleSetSelectedItem(index)}>
                                         <div className="flex items-center space-x-4">
                                             <div className="flex-1 min-w-0">
                                                 <p className="font-medium text-bcBluePrimary truncate dark:text-white">
