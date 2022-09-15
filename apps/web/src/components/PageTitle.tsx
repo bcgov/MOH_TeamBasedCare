64da5dc19fe882fb99f43e7bdd2b1c24698ff1b5
Karthikk Tamil Mani <70780157+karthikktamilmani-freshworks@users.noreply.github.com>
2022-09-15 16:11:36 -0700

    Lint updates

--- apps/web/src/components/PageTitle.tsx
+++ apps/web/src/components/PageTitle.tsx
@@ -1,20 +1,18 @@
-
-
 export interface PageTitleProps {
-    title: string;
-    description: string;
-  }
-  
+  title: string;
+  description: string;
+}
+
 export const PageTitle: React.FC<PageTitleProps> = ({ title, description }) => {
-  return (<>
-  <h1
-      tabIndex={-1}
-      className='text-bcBluePrimary text-left text-4xl text-bold focus:outline-none mt-5'
-    >
-      {title}
-    </h1>
-    <p className='text-bcBluePrimary text-left'>{description}</p>
-  </>
-    
+  return (
+    <>
+      <h1
+        tabIndex={-1}
+        className='text-bcBluePrimary text-left text-4xl text-bold focus:outline-none mt-5'
+      >
+        {title}
+      </h1>
+      <p className='text-bcBluePrimary text-left'>{description}</p>
+    </>
   );
 };
