0cdd0456b41b93db78d61a405367801a3c08ea1e
Leon <leon@freshworks.io>
2022-09-16 09:12:56 -0700

    Provide Basic Setup for Occupation Selection
    - Basic structure in Occupation
    - Empty elements in the right order
    - Some styling

--- apps/web/src/components/planning/Occupation.tsx
+++ apps/web/src/components/planning/Occupation.tsx
@@ -1,5 +1,11 @@
 import { PlanningStepHeader } from '@components';
 import { planningFormBox } from '../../styles/styles';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { SearchBar } from '../generic/SearchBar';
+import { useState } from 'react';
+import { Paginator } from '../generic/Paginator';
+import { OccupationSelector } from '../OccupationSelector';
 
 export interface OccupationProps {
   step: number;
@@ -7,9 +13,39 @@ export interface OccupationProps {
 }
 
 export const Occupation: React.FC<OccupationProps> = ({ title }) => {
+  const [occupationsSelected, setOccupationsSelected] = useState(0);
+
   return (
     <div className={planningFormBox}>
       <PlanningStepHeader>{title}</PlanningStepHeader>
+      <div className='px-5'>
+        <div className='space-y-3'>
+          <div className='space-x-1.5 flex'>
+            <FontAwesomeIcon className='inline w-6 h-6' icon={faUserCircle} />
+            <h4 className='inline text-bcBluePrimary font-bold font-sans'>
+              Select Occupation (Optional)
+            </h4>
+          </div>
+
+          <div className='space-y-2'>
+            <p className='text-sm font-extralight font-sans text-gray-400'>
+              Select all the roles on your team.
+            </p>
+            <SearchBar placeholderText='Search by keyword'></SearchBar>
+          </div>
+
+          <div className='space-y-2'>
+            <p className='text-sm font-extralight font-sans text-gray-400'>
+              {occupationsSelected} occupations selected
+            </p>
+            <Paginator></Paginator>
+  
+            <OccupationSelector></OccupationSelector>
+
+            <Paginator></Paginator>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
