faaed385167faf01360e8427045ff70bd8344e3d
Peter Pavlatos <pavlatos.peter@gmail.com>
2022-09-22 16:00:03 -0400

    TBCM-44 - Removed duplicate object for looping data of the legend

--- apps/web/src/common/constants.ts
+++ apps/web/src/common/constants.ts
@@ -100,36 +100,3 @@ export const tooltipIcons: { [key in TooltipIconTypes]: TooltipIconProps } = {
     style: 'gray-icon',
   },
 };
-
-export const activityGapLegend = [
-  {
-    faIcon: faCheckCircle,
-    color: '#2E8540',
-    text: "Care activity can be performed as part of profession's scope of practice/ provider's role description",
-  },
-  {
-    faIcon: faExclamationTriangle,
-    color: '#FCBA19',
-    text: 'Care activity can be performed with limits and conditions.',
-  },
-  {
-    faIcon: faExclamationCircle,
-    color: '#FCBA19',
-    text: 'Care activity cannot be performed BUT occupation can assist with care activity.',
-  },
-  {
-    faIcon: faTimesCircle,
-    color: '#FCBA19',
-    text: 'Care activity cannot be performed BUT occupation can assist with care activity.',
-  },
-  {
-    faIcon: faTimesCircle,
-    color: 'RED',
-    text: "Care activity cannot be performed; not part of profession's scope of practice/ provider's role description.",
-  },
-  {
-    faIcon: faQuestionCircle,
-    color: '#606060',
-    text: 'This occupation can perform some activities in this bundle with different restrictions. Please expand the activities bundle on the left for more details.',
-  },
-];
