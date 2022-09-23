56bf20c062b86b2f553d06deb4751faa48010ce5
Leon <leon@freshworks.io>
2022-09-23 12:26:08 -0700

    Provide Initial Functionality for CSV Convert

--- apps/api/src/example-data/convert-exported-data.js
+++ apps/api/src/example-data/convert-exported-data.js
@@ -4,8 +4,41 @@ const data = require('./form-export.example.json');
 
 const main = () => {
   try {
-    console.log(data);
-  } catch (err) {}
+    const f = data.headers;
+    const fields = f.map(element => {
+      if (element === 'Activities Bundle') return { label: 'Activities Bundle', value: 'name' };
+      return element;
+    });
+
+    const emptyRow = {
+      name: '',
+      'Respiratory Therapist': '',
+      'Speech Language Pathologist': '',
+      'Occupational Therapist': '',
+      'Registered Nurse - Critical Care': '',
+      'Registered Nurse - Medical/ Surgical': '',
+      'Licensed Practical Nurse (LPN)': '',
+      'Health Care Assistant (HCA)': '',
+      'Other - Non Clinical': '',
+      Physician: '',
+      numberOfGaps: '',
+    };
+
+    const opts = { fields };
+    const resData = data.data
+      .map(element => {
+        const { careActivities, ...remainder } = element;
+        const res = [remainder, ...careActivities, emptyRow];
+        return [{ ...emptyRow, name: remainder.name }, ...careActivities, emptyRow];
+      })
+      .flat();
+
+    let csv = parse(resData, opts);
+    // console.log(csv);
+    fs.writeFileSync('./src/example-data/converted-data.csv', csv);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 main();
