5d7919cf6f14758304fadc079ea485b75adf129b
Leon <leon@freshworks.io>
2022-09-26 11:15:10 -0700

    Add Convert Logic in a Function

---
+++ apps/api/src/common/convert-activity-gap-table-to-csv.ts
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const { parse } = require('json2csv');
+
+interface CareActivityProps {
+  name: string;
+  'Respiratory Therapist': string;
+  'Speech Language Pathologist': string;
+  'Occupational Therapist': string;
+  'Registered Nurse - Critical Care': string;
+  'Registered Nurse - Medical/ Surgical': string;
+  'Licensed Practical Nurse (LPN)': string;
+  'Health Care Assistant (HCA)': string;
+  'Other - Non Clinical': string;
+  Physician: string;
+  numberOfGaps?: string;
+}
+interface JsonDataProps extends CareActivityProps {
+  careActivities: CareActivityProps[];
+}
+
+export const convertActivityGapTableToCSV = (data: any) => {
+  try {
+    const fields = data.headers.map((element: string | { label: string; value: string }) => {
+      if (element === 'Activities Bundle') return { label: 'Activities Bundle', value: 'name' };
+      return element;
+    });
+
+    const emptyRow: CareActivityProps = {
+      name: '',
+      'Respiratory Therapist': '',
+      'Speech Language Pathologist': '',
+      'Occupational Therapist': '',
+      'Registered Nurse - Critical Care': '',
+      'Registered Nurse - Medical/ Surgical': '',
+      'Licensed Practical Nurse (LPN)': '',
+      'Health Care Assistant (HCA)': '',
+      'Other - Non Clinical': '',
+      Physician: '',
+      numberOfGaps: '',
+    };
+    const options = { fields };
+
+    const resultData = data.data
+      .map((element: JsonDataProps) => {
+        const { careActivities, ...remainder } = element;
+        const res = [remainder, ...careActivities, emptyRow];
+        return [{ ...emptyRow, name: remainder.name }, ...careActivities, emptyRow];
+      })
+      .flat();
+
+    const csv = parse(resultData, options);
+    fs.writeFileSync('./src/example-data/converted-data.csv', csv);
+  } catch (err) {
+    console.log(err);
+  }
+};
