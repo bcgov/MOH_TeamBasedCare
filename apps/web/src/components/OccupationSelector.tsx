413bc2a199c5a9f42279c79937441320b268eac2
Karthikk Tamil Mani <70780157+karthikktamilmani-freshworks@users.noreply.github.com>
2022-09-29 11:03:55 -0700

    Select All for occupation (#42)

--- apps/web/src/components/OccupationSelector.tsx
+++ apps/web/src/components/OccupationSelector.tsx
@@ -1,20 +1,45 @@
 import { useOccupations } from 'src/services/useOccupations';
 import { OccupationItem } from './OccupationItem';
 import { isOdd } from 'src/common/util';
+import { useFormikContext } from 'formik';
 
 export const OccupationSelector = ({}) => {
   const { occupations } = useOccupations();
-
+  const { values, setFieldValue } = useFormikContext<any>();
   return (
-    <div className='flex-1 flex flex-col'>
-      {occupations.map((occupation, index) => {
-        const styling = isOdd(index) ? 'occupation-item-box-gray' : 'occupation-item-box-white';
-        return (
-          <div key={index} className={`occupation-item-box ${styling}`}>
-            <OccupationItem key={occupation.id} {...occupation}></OccupationItem>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div className='flex items-center p-4'>
+        <input
+          type='checkbox'
+          name='selectAll'
+          id='selectAll'
+          className='mr-3 h-5 w-5 min-w-5 accent-bcBlueLink'
+          onChange={(e: any) => {
+            if (e.target.checked) {
+              setFieldValue(
+                'occupation',
+                occupations.map((e: any) => e.id),
+              );
+            } else {
+              setFieldValue('occupation', []);
+            }
+          }}
+          checked={values.occupation.length === occupations.length}
+        />
+        <label className='font-bold' htmlFor={'selectAll'}>
+          Select all
+        </label>
+      </div>
+      <div className='flex-1 flex flex-col'>
+        {occupations.map((occupation, index) => {
+          const styling = isOdd(index) ? 'occupation-item-box-gray' : 'occupation-item-box-white';
+          return (
+            <div key={index} className={`occupation-item-box ${styling}`}>
+              <OccupationItem key={occupation.id} {...occupation}></OccupationItem>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
