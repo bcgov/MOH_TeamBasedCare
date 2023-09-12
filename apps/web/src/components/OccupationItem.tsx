8ca7413fdae3c63dc623b413caff3708a718aef9
Kushal Arora <kushal.arora1@ca.ey.com>
2023-09-12 10:34:31 -0700

    Remove commented code for getting rid of Occupation tags

--- apps/web/src/components/OccupationItem.tsx
+++ apps/web/src/components/OccupationItem.tsx
@@ -3,9 +3,6 @@ import { OccupationItemProps } from 'src/common/interfaces';
 import React from 'react';
 
 export const OccupationItem = ({ id, displayName }: OccupationItemProps) => {
-  // const tagText = isRegulated ? 'Regulated' : 'Unregulated';
-  // const tagColor = isRegulated ? TagVariants.BLUE : TagVariants.GREEN;
-
   return (
     <div className='flex flex-1 items-center'>
       <div className='flex-initial w-full'>
@@ -16,14 +13,6 @@ export const OccupationItem = ({ id, displayName }: OccupationItemProps) => {
           label={displayName}
         ></Checkbox>
       </div>
-
-      {/** No need to show occupation tag: All occupations are regulated for now. */}
-      {/** changes: (1/2) w-5/6 changed to w-full for Checkbox's parent div */}
-      {/** changes: (2/2) commented Tag element */}
-
-      {/* <div className='flex flex-initial w-1/6 justify-end'>
-        <Tag text={tagText} tagStyle={tagColor}></Tag>
-      </div> */}
     </div>
   );
 };
