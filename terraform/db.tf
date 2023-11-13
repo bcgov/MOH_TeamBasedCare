2e2e877b2302893dc1913d623588cbd2571ea8c0
Kushal Arora <kushal.arora1@ca.ey.com>
2023-11-13 15:29:38 -0800

    [TBCM-152] [Build fix] updated tf aws version to latest minor version

--- terraform/db.tf
+++ terraform/db.tf
@@ -16,7 +16,7 @@ resource "aws_rds_cluster_instance" "pgsql" {
 resource "aws_rds_cluster" "pgsql" {
   cluster_identifier  = local.db_name
   engine              = "aurora-postgresql"
-  engine_version      = "14.9"
+  engine_version      = "15.4"
   availability_zones  = ["ca-central-1a", "ca-central-1b"]
   database_name       = replace(var.project_code, "-", "_")
   master_username     = var.db_username
