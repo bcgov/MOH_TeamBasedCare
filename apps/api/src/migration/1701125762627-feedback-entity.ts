83771279e90d14eb62dc041fb27e0b6457fdcf2a
Kushal Arora <kushal.arora1@ca.ey.com>
2023-11-28 11:53:21 -0800

    [TBCM-158] [API] formatted the migration to make it more readable

--- apps/api/src/migration/1701125762627-feedback-entity.ts
+++ apps/api/src/migration/1701125762627-feedback-entity.ts
@@ -5,7 +5,17 @@ export class feedbackEntity1701125762627 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "feedback" ("created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "id" uuid NOT NULL DEFAULT uuid_generate_v4(), "text" character varying(4096) NOT NULL, "created_by_email" character varying(255) NOT NULL, "created_by_name" character varying(255) NOT NULL, "created_by_username" character varying(255) NOT NULL, CONSTRAINT "PK_8389f9e087a57689cd5be8b2b13" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "feedback" (
+        "created_at" TIMESTAMP NOT NULL DEFAULT now(), 
+        "updated_at" TIMESTAMP NOT NULL DEFAULT now(), 
+        "id" uuid NOT NULL DEFAULT uuid_generate_v4(), 
+        "text" character varying(4096) NOT NULL, 
+        "created_by_email" character varying(255) NOT NULL, 
+        "created_by_name" character varying(255) NOT NULL, 
+        "created_by_username" character varying(255) NOT NULL, 
+        CONSTRAINT "PK_8389f9e087a57689cd5be8b2b13" 
+        PRIMARY KEY ("id")
+      )`,
     );
   }
 
