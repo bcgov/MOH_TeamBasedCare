2df76de22c9d578369bea348737bb6952517403f
Kushal Arora <kushal.arora1@ca.ey.com>
2023-12-15 13:08:45 -0800

    [TBCM-175] format migration for readability

--- apps/api/src/migration/1702504051954-care-activity-search-term.ts
+++ apps/api/src/migration/1702504051954-care-activity-search-term.ts
@@ -5,7 +5,17 @@ export class careActivitySearchTerm1702504051954 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "care_activity_search_term" ("created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "id" uuid NOT NULL DEFAULT uuid_generate_v4(), "term" character varying(255) NOT NULL, "created_by" character varying(255) NOT NULL, "created_by_username" character varying(255) NOT NULL, "created_by_name" character varying(255) NOT NULL, "created_by_email" character varying(255) NOT NULL, CONSTRAINT "PK_5570a5a1d7e56ad507ac9052d78" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "care_activity_search_term" (
+        "created_at" TIMESTAMP NOT NULL DEFAULT now(),
+        "updated_at" TIMESTAMP NOT NULL DEFAULT now(),
+        "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+        "term" character varying(255) NOT NULL,
+        "created_by" character varying(255) NOT NULL,
+        "created_by_username" character varying(255) NOT NULL,
+        "created_by_name" character varying(255) NOT NULL,
+        "created_by_email" character varying(255) NOT NULL,
+        CONSTRAINT "PK_5570a5a1d7e56ad507ac9052d78" PRIMARY KEY ("id")
+      )`,
     );
   }
 
