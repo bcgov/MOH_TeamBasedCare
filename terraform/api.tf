40acb36b30e515c09393205e6e2a2b9aa9add89d
Kushal Arora <kushal.arora1@ca.ey.com>
2024-01-04 16:34:06 -0800

    [TBCM-183] Migrate away from legacy Origin Access identity to Origin Access Control for the App distribution

--- terraform/api.tf
+++ terraform/api.tf
@@ -10,7 +10,7 @@ resource "aws_s3_bucket" "api" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.api_kms_key.id
+        kms_master_key_id = aws_kms_key.api_kms_key.arn
         sse_algorithm = "aws:kms"
       }
     }
