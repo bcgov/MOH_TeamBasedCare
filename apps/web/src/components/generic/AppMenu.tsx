dee7b111e026309faf791c55670aed8996677435
Kushal Arora <kushal.arora1@ca.ey.com>
2024-02-08 14:31:02 -0800

    update text and style

--- apps/web/src/components/generic/AppMenu.tsx
+++ apps/web/src/components/generic/AppMenu.tsx
@@ -32,7 +32,7 @@ export const AppMenu: React.FC<AppMenuProps> = ({
     <>
       <div
         className={`absolute right-0 z-10 mt-2 ${
-          size === 'lg' ? 'w-96' : 'w-56'
+          size === 'lg' ? 'w-96' : 'w-64'
         } origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`}
         role='menu'
         aria-orientation='vertical'
