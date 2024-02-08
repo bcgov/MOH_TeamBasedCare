a1b9511f4a3c9e78f157fc7733979f998bd0e86a
Kushal Arora <kushal.arora1@ca.ey.com>
2024-02-08 13:56:26 -0800

    text update

--- apps/web/src/components/UserDropdown.tsx
+++ apps/web/src/components/UserDropdown.tsx
@@ -40,7 +40,7 @@ export const UserDropdown = () => {
     {
       items: [
         { title: 'Feedback', onClick: onFeedbackClick, color: 'blue' },
-        { title: 'User guide', onClick: onUserGuideClick, color: 'blue' },
+        { title: 'User manual', onClick: onUserGuideClick, color: 'blue' },
         { title: 'Logout', onClick: logout, color: 'red' },
       ],
     },
@@ -78,7 +78,7 @@ export const UserDropdown = () => {
       <ModalWrapper
         isOpen={showUserGuideModal}
         setIsOpen={setShowUserGuideModal}
-        title={'User guide'}
+        title={'User manual'}
         closeButton={{ title: 'Close' }}
       >
         <div className='p-4'>
