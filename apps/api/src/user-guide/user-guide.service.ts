e8a6cbb6029948470bd9a128f4c9a01545111c7e
gperkinscgi <greg.perkins@cgi.com>
2025-09-12 10:22:12 -0700

    JSU-44 AWS SDK V3

--- apps/api/src/user-guide/user-guide.service.ts
+++ apps/api/src/user-guide/user-guide.service.ts
@@ -1,4 +1,5 @@
-import AWS from 'aws-sdk';
+import { S3Client, ListObjectsCommand, GetObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { Inject, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { UserGuide } from '@tbcm/common';
 import { AppLogger } from 'src/common/logger.service';
@@ -7,24 +8,21 @@ const BUCKET_NAME = process.env.DOCS_BUCKET ?? 'tbcm-dev-docs';
 
 @Injectable()
 export class UserGuideService {
-  private s3: AWS.S3 | null = null;
+  private s3Client: S3Client | null = null;
 
   constructor(@Inject(Logger) private readonly logger: AppLogger) {
     if (process.env.DOCS_BUCKET) {
-      this.s3 = new AWS.S3({
-        params: {
-          Bucket: BUCKET_NAME,
-        },
-      });
+      this.s3Client = new S3Client({});
     }
   }
 
   async findAll() {
-    if (!this.s3) {
+    if (!this.s3Client) {
       throw new InternalServerErrorException('the feature is disabled');
     }
     try {
-      const result = await this.s3.listObjects().promise();
+      const command = new ListObjectsCommand({ Bucket: BUCKET_NAME });
+      const result = await this.s3Client.send(command);
 
       return (
         result.Contents?.map(o => {
@@ -38,18 +36,17 @@ export class UserGuideService {
   }
 
   async getSignedUrl(key: string, version?: string) {
-    if (!this.s3) {
+    if (!this.s3Client) {
       throw new InternalServerErrorException('the feature is disabled');
     }
     try {
-      const params = {
+      const command = new GetObjectCommand({
         Bucket: BUCKET_NAME,
         Key: key,
         VersionId: version,
-        Expires: 60,
         ResponseContentDisposition: 'attachment',
-      };
-      return await this.s3.getSignedUrlPromise('getObject', params);
+      });
+      return await getSignedUrl(this.s3Client, command, { expiresIn: 60 });
     } catch (e) {
       this.logger.error(e, 'S3');
       throw new InternalServerErrorException('failed to get the signed url of a user guide');
