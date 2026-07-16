f755190de0076d306940e23702746d7595d79e1c
chloe-yuu <84537348+chloe-yuu@users.noreply.github.com>
2026-07-16 13:50:41 -0700

    upgrade aurora-postgresql from 15.12 to 15.15

--- terraform/db.tf
+++ terraform/db.tf
@@ -16,7 +16,7 @@ resource "aws_rds_cluster_instance" "pgsql" {
 resource "aws_rds_cluster" "pgsql" {
   cluster_identifier          = local.db_name
   engine                      = "aurora-postgresql"
-  engine_version              = "15.12"
+  engine_version              = "15.15"
   allow_major_version_upgrade = true
   apply_immediately           = true
   availability_zones          = ["ca-central-1a", "ca-central-1b"]
